18da8829c978a5a17adef9db7ca05789b04b9638
Juan <jagames145@hotmail.com>
2022-08-15 23:48:55 -0500

    Parcialmente completado, falta manager de Ordenes

---
+++ init.py
@@ -0,0 +1,10 @@
+import Order
+import Paquetes
+import Person 
+ 
+
+def mainMenu():
+    print("****SERVICES**********")
+    print("1. Añadir Interno. . . ")
+    print("2. Añadir Customer. . . ")
+    print("3. Añadir Orden. . .")
